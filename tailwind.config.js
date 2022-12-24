703776613ad680f576bf5440353887c13ea583c7
ananiya0000 <ananigd0@gmail.com>
2022-12-24 21:11:01 +0300

    finished page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        latoLight:"LatoLight",
+        latoBold:"LatoBold",
+        latoReg:"LatoReg"
+      }
+    },
+  },
+  plugins: [],
+}
